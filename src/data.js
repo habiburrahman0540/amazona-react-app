3695e5ca79e4b7ebe35b042bc096a25f69722f8e
unknown <habiburrahman0540@gmail.com>
2021-11-18 04:46:27 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,104 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/p-2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:12,
+            description:'This product is high quality.'
+        },
+        {
+            _id:'2',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/p-2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:12,
+            description:'This product is high quality.'
+        },
+        {
+            _id:'3',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/p-2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:12,
+            description:'This product is high quality.'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/p-2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:12,
+            description:'This product is high quality.'
+        },
+        {
+            _id:'5',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/p-2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:12,
+            description:'This product is high quality.'
+        },
+        {
+            _id:'6',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/p-2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:12,
+            description:'This product is high quality.'
+        },
+        {
+            _id:'7',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/p-2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:12,
+            description:'This product is high quality.'
+        },
+        {
+            _id:'8',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/p-2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:12,
+            description:'This product is high quality.'
+        },
+        {
+            _id:'9',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/p-2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:12,
+            description:'This product is high quality.'
+        },
+    ]
+}
+export default data;
